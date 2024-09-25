7951f0adcc00e567c32f07f26c9ba559a298e76e
masamichi <133827507+masamichi2004@users.noreply.github.com>
2024-09-25 18:26:46 +0900

    Add example.txt with JSON data

---
+++ benchpress/benchpress-2024-09-25.rs
@@ -0,0 +1,16 @@
+{
+  "sets": [
+    {
+      "weight": 16,
+      "reps": 15
+    },
+    {
+      "weight": 16,
+      "reps": 15
+    },
+    {
+      "weight": 13,
+      "reps": 10
+    }
+  ]
+}
